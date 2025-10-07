eefc08b750f80f3eb91d0adf456528036ff7669c
gmgori3 <gmgori@gmail.com>
2025-10-08 00:09:41 +0530

    changs

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const { default: mongoose } = require("mongoose");
 
 dotenv.config(); // Load .env
 
@@ -35,7 +36,20 @@ app.get("/", (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`🚀 Server running on http://localhost:${PORT}`);
+// });
+
+let isConnected = false;
+async function connectToDatabase() {
+  try {
+    if (!isConnected) {
+      await mongoose.connect();
+      isConnected = true;
+      console.log("Database connected successfully.");
+  }
+} catch (error) {
+  console.error("Database connection error:", error);
+}
+}
