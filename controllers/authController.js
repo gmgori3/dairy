475986c406c351cd9c864192aba3fcbc7d1c582e
gmgori3 <gmgori@gmail.com>
2025-10-12 11:40:27 +0530

    chanegs

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,7 +13,7 @@ const generateToken = (user) => {
 // ✅ Register Dairy
 exports.registerDairy = async (req, res) => {
   try {
-    const { firstName, countryCode, phoneNumber, centerName, dateOfBirth } = req.body;
+    const { first_name, countryCode, phoneNumber, centerName, dateOfBirth } = req.body;
 
     const existing = await User.findOne({ phoneNumber });
     if (existing) {
